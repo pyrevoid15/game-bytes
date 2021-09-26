50e337117a3263e881f1d941e106cfcdda39be79
Btmodz1 <91026840+Btmodz1@users.noreply.github.com>
2021-09-26 17:57:55 -0400

    Created beatmap file and started working on implementing note spawning

---
+++ Crucible/Assets/Minigames/Tempo/Scripts/DrumNoteSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrumNoteSpawner : MonoBehaviour
+{
+    public float startTime;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (BeatMap.drumNoteInfo[0].time - (Time.time - startTime) < BeatMap.approachTime) {
+            // spawn note
+            BeatMap.drumNoteInfo.RemoveAt(0);
+        }
+    }
+}
