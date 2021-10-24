cb4a5d191d872aa7bb0f2fa0a03be383e25b645e
Emily Amspoker <46005655+eamspoker@users.noreply.github.com>
2021-10-24 16:18:00 -0400

    created results scene

---
+++ Crucible/Assets/Minigames/Tempo/Scripts/GetScores.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GetScores : MonoBehaviour
+{
+    public TextMeshProUGUI player1;
+    public TextMeshProUGUI player1_det;
+
+    public TextMeshProUGUI player2;
+    public TextMeshProUGUI player2_det;
+
+    public TextMeshProUGUI total_score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // getting player1's scores
+        float total1 = Score.player1Score;
+        float max1 = Score.player1MaxScore;
+
+        float percent = ((total1/max1)*100);
+        percent = Mathf.Round(percent);
+
+        player1.text = percent.ToString() + "%";
+        string miss = " misses";
+        if(max1-total1 == 1) miss = " miss";
+        player1_det.text = (max1-total1).ToString() + miss;
+
+        // getting player 2's scores
+        float total2 = Score.player2Score;
+        float max2 = Score.player2MaxScore;
+
+        float percent2 = ((total2/max2)*100);
+        percent2 = Mathf.Round(percent2);
+
+        player2.text = percent2.ToString() + "%";
+        string miss2 = " misses";
+        if(max1-total1 == 1) miss2 = " miss";
+        player2_det.text = (max2-total2).ToString() + miss2;
+
+
+        float group_total = total1 + total2;
+        float group_max = max1 + max2;
+        float percent3 = Mathf.Round((group_total/group_max)*100);
+
+        total_score.text = percent3.ToString() + "%";
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Crucible/Assets/Minigames/Tempo/Scripts/scoreDummy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score
+{
+    public static float player1Score = 1;
+    public static float player1MaxScore = 3;
+    public static float player2Score = 3;
+    public static float player2MaxScore = 4;
+}
